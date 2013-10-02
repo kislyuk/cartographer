5d142133f7f03af80010cdc884b8e2d05e1c287c
Andrey Kislyuk <kislyuk@gmail.com>
2013-10-02 01:54:12 -0700

    Continue Cartographer

--- postproc_db.py
+++ postproc_db.py
@@ -4,7 +4,7 @@ import os, sys, logging, urllib, time, string, json, argparse, collections, date
 from concurrent.futures import ThreadPoolExecutor, wait
 import lz4
 
-pool = ThreadPoolExecutor(max_workers=8)
+pool = ThreadPoolExecutor(max_workers=16)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -35,7 +35,7 @@ def compute_occlusions(box):
         p1.save()
     print("Finished", SW, NE, len(points))
 
-step = 4
+step = 2
 boxes = []
 for lat in range(-90, 90, step):
     for lng in range(-180, 180, step):
@@ -44,32 +44,8 @@ for lat in range(-90, 90, step):
 for result in pool.map(compute_occlusions, boxes):
     pass
 
-'''
-        futures.append(pool.submit(compute_occlusions((lng, lat), (lng+step, lat+step))
-        futures = []
-        points = list(POI.objects(at__geo_within_box=((lng, lat), (lng+step, lat+step))))
-        for i, p1 in enumerate(points):
-            for j, p2 in enumerate(points[i+1:]):
-                coords1, coords2 = p1.at['coordinates'], p2.at['coordinates']
-                dist = math.sqrt(abs(coords1[0]-coords2[0])**2 + abs(coords1[1]-coords2[1])**2)
-
-                occluded_point = p1 if p1.rank < p2.rank else p2
-                for zoom, spacing in enumerate(zoomspacing):
-                    if dist < spacing:
-                        continue
-                    break
-                occluded_point.min_zoom = max(occluded_point.min_zoom, zoom)
-                #futures.append(saver.submit(occluded_point.save))
-            p1.save()
-        #wait(futures)
-        print(lat, lng, len(points))
-'''
 #        docs_by_rank = sorted(POI.objects(at__geo_within_center=(doc.at['coordinates'], spacing)),
 #                              key=lambda point: point.rank or 0,
 #                              reverse=True)
-#		for doc in POI.objects(at__geo_within_center=(doc.at['coordinates'], 1)):
-#			print("\t", doc.name, doc.min_zoom)
-#        for doc in docs_by_rank:
 #    for doc in POI.objects(at__geo_within_center=(doc.at['coordinates'], 1), min_zoom__gt=0).order_by('-rank'):
-#    for doc in POI.objects(at__geo_within_center=(doc.at['coordinates'], 1), min_zoom=99).order_by('-rank'):
 #    for doc2 in POI.objects(at__geo_within_center=(doc.at['coordinates'], zoomspacing[doc.min_zoom]), min_zoom__lte=doc.min_zoom).order_by('-rank'):
